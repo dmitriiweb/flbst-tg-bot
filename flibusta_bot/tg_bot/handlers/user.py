acf1ec2e83245781f4d549a58d70687df1e52791
Dmitrii K <dmitriik@protonmail.com>
2025-05-31 03:06:45 +0700

    improve start command

--- flibusta_bot/tg_bot/handlers/user.py
+++ flibusta_bot/tg_bot/handlers/user.py
@@ -1,12 +1,26 @@
-from aiogram import Router
+from aiogram import F, Router
 from aiogram.filters.command import Command
+from aiogram.types import Message
+from aiogram.enums import ChatType
+
+from flibusta_bot.db import db_session, storage
 
 router = Router()
+router.message.filter(F.chat.type == ChatType.PRIVATE)
 
 
 @router.message(Command("start"))
 async def cmd_start(message):
+    async with db_session() as session:
+        _ = await storage.users.get_or_create_user(
+            session, message.from_user.id, message
+        )
     await message.answer(
         "Привет! Я бот для поиска книг в библиотеке. "
         "Отправь мне название книги или автора, и я помогу тебе найти нужную информацию."
     )
+
+
+@router.message(F.text)
+async def search_books(message: Message):
+    pass
