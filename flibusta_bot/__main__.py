f9d7f50497ec5666ba0f3c62d828400a6eb47b14
Dmitrii K <dmitriik@protonmail.com>
2025-06-08 16:38:18 +0700

    clear code

--- flibusta_bot/__main__.py
+++ flibusta_bot/__main__.py
@@ -1,17 +1,5 @@
 from .tg_bot import App
 
-"""
-# from flibusta_bot.flibusta_parser.app import App as ParserApp
-
-    query = "хаос"
-    async with ParserApp() as app:
-        search_results = await app.search_book(query)
-        print(search_results[0])
-        print()
-        print(search_results[1])
-
-"""
-
 
 def main():
     app = App()
